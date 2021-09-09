248bf59cfcc6e37d8f28ed440bc4b3e0334a8c5a
Gino Rosati <littlerosati323@yahoo.com>
2021-09-09 12:32:19 -0400

    Thursday before lunch

---
+++ EmployeesApi/Controllers/Employees.cs
@@ -0,0 +1,43 @@
+﻿using EmployeesApi.Data;
+using EmployeesApi.Models.Employees;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class Employees : ControllerBase
+    {
+        private readonly EmployeesDataContext _context;
+
+        public Employees(EmployeesDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("employees")]
+        public async  Task<ActionResult> GetAllEmployees()
+        {
+            var employees = await _context.Employees
+                .Where(e => e.IsActive)
+                .Select(e => new GetEmployeeResponseItem
+                {
+                    Id = e.Id,
+                    FirstName = e.FirstName,
+                    Department = e.Department,
+                    LastName = e.LastName
+                })
+                .ToListAsync();
+
+            var response = new GetEmployeeResponse
+            {
+                Data = employees
+            };
+
+            return Ok(response);
+        }
+    }
+}
--- EmployeesApi/Startup.cs
+++ EmployeesApi/Startup.cs
@@ -1,7 +1,9 @@
+using EmployeesApi.Data;
 using EmployeesApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,10 @@ namespace EmployeesApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EmployeesDataContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("employees"));
+            });
             services.AddFeatureManagement(Configuration.GetSection("flags"));
             services.AddHttpClient<ApiOnCallService>(options =>
             {
